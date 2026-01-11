b86af69131f3af6e5e0edd26ba18fb19a192b2bc
IbrahimFullStackWebDev <ibrahim.fullstack.dev@gmail.com>
2026-01-11 21:44:59 +0300

    feat: complete responsive navbar with mobile menu toggle, and add images to  assets folder

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,22 @@
-import "./App.css";
-
-const name: boolean = true;
+import { Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
+import Doctors from "./pages/Doctors";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
 
 function App() {
-  return <>{name}</>;
+  return (
+    <div>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/doctors" element={<Doctors />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -1,7 +1,68 @@
-import React from "react";
+import React, { useState } from "react";
+import { assets } from "../assets/assets.ts";
+import { NavLink } from "react-router-dom";
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  const [showMenu, setShowMenu] = useState<boolean>(false);
+
+  interface NavItems {
+    path: string;
+    lable: string;
+  }
+  const navitems: NavItems[] = [
+    { path: "/", lable: "HOME" },
+    { path: "/doctors", lable: "ALL DOCTORS" },
+    { path: "/about", lable: "ABOUT" },
+    { path: "/contact", lable: "CONTACT" },
+  ];
+
+  return (
+    <>
+      <div className="mx-4 lg:mx-14 flex flex-row items-center justify-between lg:px-6 py-4 border-b border-gray-500">
+        <img src={assets.logo} className="w-45" alt="Logo" />
+        <ul className="hidden md:flex flex-row items-center gap-6 text-gray-700 font-semibold">
+          {navitems.map((item) => (
+            <NavLink key={item.path} to={item.path}>
+              <p className="text-gray-700 font-semibold">{item.lable}</p>
+              <hr className="w-full border-none h-0.5 bg-gray-500 hidden" />
+            </NavLink>
+          ))}
+        </ul>
+        <button
+          type="button"
+          className="px-8 py-3 bg-blue-500 text-white rounded-full cursor-pointer hover:bg-blue-700 transition-all duration-300 hidden md:block"
+        >
+          Create an account
+        </button>
+        <img
+          onClick={() => setShowMenu((prev) => !prev)}
+          src={showMenu ? assets.cross_icon : assets.menu_icon}
+          className="w-6 md:hidden mr-4 cursor-pointer"
+          alt="Menu"
+        />
+      </div>
+      <div
+        className={`md:hidden w-full h-screen transition-all duration-500 ${
+          showMenu ? "block" : "hidden"
+        }`}
+      >
+        <ul className="flex flex-col items-center mt-10 gap-6 text-gray-700 font-semibold">
+          {navitems.map((item) => (
+            <NavLink
+              key={item.path}
+              to={item.path}
+              className={"group"}
+              onClick={() => setShowMenu((prev) => !prev)}
+            >
+              <p className="text-gray-700 font-semibold group-[.active]:bg-blue-500 group-[.active]:text-white px-8 py-2 rounded-md">
+                {item.lable}
+              </p>
+            </NavLink>
+          ))}
+        </ul>
+      </div>
+    </>
+  );
 };
 
 export default Navbar;
