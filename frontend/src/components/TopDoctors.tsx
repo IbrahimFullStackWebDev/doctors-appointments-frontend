5538f3771ea57d43d0140b0963063d1d9dc28db2
IbrahimFullStackWebDev <ibrahim.fullstack.dev@gmail.com>
2026-01-25 08:49:30 +0300

    optime:delete extra conditions and use the dynamic className depending on doctor availability and add object cover className for make sure that all image the same size

--- frontend/src/components/TopDoctors.tsx
+++ frontend/src/components/TopDoctors.tsx
@@ -16,39 +16,37 @@ const TopDoctors = () => {
       </div>
 
       <div className="w-full grid grid-cols-[repeat(auto-fill,minmax(200px,1fr))] gap-6">
-        {doctors.slice(0, 10).map((item: DoctorDataType) => (
-          <div
-            key={item.id}
-            className="w-full flex flex-col items-start gap-4 pb-4 border border-gray-300 rounded-lg cursor-pointer hover:translate-y-[-10px] transition-all duration-500"
-            onClick={() => {
-              navigate(`/appointments/${item.id}`);
-              scrollTo(0, 0);
-            }}
-          >
-            <img
-              src={item.image}
-              className="bg-blue-100 rounded-lg"
-              alt="doctor image"
-            />
-            <div className="flex flex-col items-start gap-2 px-4">
-              <div className="flex flex-row items-center gap-2">
-                {item.available ? (
-                  <>
-                    <div className="p-1 rounded-full bg-green-500"></div>
-                    <p className="text-sm text-gray-500">Available</p>
-                  </>
-                ) : (
-                  <>
-                    <div className="p-1 rounded-full bg-red-500"></div>
-                    <p className="text-sm text-gray-500">Unavailable</p>
-                  </>
-                )}
+        {doctors &&
+          doctors.slice(0, 10).map((item: DoctorDataType) => (
+            <div
+              key={item.id}
+              className="w-full flex flex-col items-start gap-4 pb-4 border border-gray-300 rounded-lg cursor-pointer hover:translate-y-[-10px] transition-all duration-500"
+              onClick={() => {
+                navigate(`/appointments/${item.id}`);
+                scrollTo(0, 0);
+              }}
+            >
+              <img
+                src={item.image}
+                className="bg-blue-100 rounded-lg object-cover"
+                alt="doctor image"
+              />
+              <div className="flex flex-col items-start gap-2 px-4">
+                <div className="flex flex-row items-center gap-2">
+                  <div
+                    className={`${item.available ? "bg-green-500" : "bg-red-500"} p-1 rounded-full`}
+                  ></div>
+                  <p
+                    className={`${item.available ? "text-green-500" : "text-red-500"} text-sm`}
+                  >
+                    {item.available ? "Available" : "Unavailable"}
+                  </p>
+                </div>
+                <p className="text-xl">{item.name}</p>
+                <p className="text-gray-500">{item.speciality}</p>
               </div>
-              <p className="text-xl">{item.name}</p>
-              <p className="text-gray-500">{item.speciality}</p>
             </div>
-          </div>
-        ))}
+          ))}
       </div>
       <button
         className="bg-blue-100 px-14 py-3 rounded-full cursor-pointer"
