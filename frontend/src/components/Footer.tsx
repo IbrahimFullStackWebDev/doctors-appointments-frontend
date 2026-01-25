1fc2b16a217212c5108e218e8afa723309f9dcfd
IbrahimFullStackWebDev <ibrahim.fullstack.dev@gmail.com>
2026-01-25 09:07:05 +0300

    make email and phone number enteractive and home and about us phrases

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -1,9 +1,9 @@
-import React from "react";
 import { assets } from "../assets/assets";
+import { Link } from "react-router-dom";
 
 const Footer = () => {
   return (
-    <footer className="mt-100 mb-20 flex flex-col items-center gap-10">
+    <footer className="mt-20 xl:mt-50 mb-10 flex flex-col items-center gap-10">
       <div className="w-full flex flex-col sm:flex-row items-start justify-between gap-10">
         <div className="flex flex-col items-start gap-8">
           <img src={assets.logo} alt="logo" className="w-45" />
@@ -17,11 +17,23 @@ const Footer = () => {
             scrambled it to make a type specimen book.
           </p>
         </div>
-        <div className="flex flex-col items-strat gap-8">
+        <div className="flex flex-col items-start gap-8">
           <p className="font-medium text-xl">COMPANY</p>
           <div className="flex flex-col items-start gap-2 text-gray-700 text-sm">
-            <p>Home</p>
-            <p>About us</p>
+            <Link
+              to="/"
+              onClick={() => window.scrollTo(0, 0)}
+              className="hover:text-blue-500 transition-all"
+            >
+              Home
+            </Link>
+            <Link
+              to="/about"
+              onClick={() => window.scrollTo(0, 0)}
+              className="hover:text-blue-500 transition-all"
+            >
+              About us
+            </Link>
             <p>Delivery</p>
             <p>Privacy policy</p>
           </div>
@@ -29,8 +41,19 @@ const Footer = () => {
         <div className="flex flex-col items-strat gap-8">
           <p className="font-medium text-xl">GET IN TOUCH</p>
           <div className="flex flex-col items-start gap-2 text-gray-700 text-sm">
-            <p>+0-000-000-000</p>
-            <p>ibrahim.fullstack.dev@gmail.com</p>
+            <a
+              href="tel:+967778099847"
+              className="hover:text-blue-600 transition-all duration-300 flex items-center gap-2"
+            >
+              <span className="text-lg">📞</span> +967-778-099-847
+            </a>
+            <a
+              href="mailto:ibrahim.fullstack.dev@gmail.com"
+              className="hover:text-blue-600 transition-all duration-300 flex items-center gap-2"
+            >
+              <span className="text-lg">✉️</span>
+              ibrahim.fullstack.dev@gmail.com
+            </a>
           </div>
         </div>
       </div>
--- frontend/src/components/TopDoctors.tsx
+++ frontend/src/components/TopDoctors.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import { useNavigate } from "react-router-dom";
 import { type DoctorDataType } from "../types/index";
 import { useAppContext } from "../context/AppContext";
