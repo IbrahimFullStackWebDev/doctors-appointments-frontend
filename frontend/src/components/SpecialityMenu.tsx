68b78a50dc9a9f16a374d9ad2bb5b1501c5b734d
IbrahimFullStackWebDev <ibrahim.fullstack.dev@gmail.com>
2026-01-26 11:12:20 +0300

    fix: Makeing scroll appear horizontally

--- frontend/src/components/SpecialityMenu.tsx
+++ frontend/src/components/SpecialityMenu.tsx
@@ -6,25 +6,27 @@ const SpecialityMenu = () => {
   const navigate = useNavigate();
 
   return (
-    <div className="w-full flex flex-row items-center justify-center px-8 gap-4 p-6 overflow-x-auto">
-      {specialityData.map((item: SpecialityItem) => (
-        <div
-          key={item.id}
-          className="flex-shrink-0 flex flex-col items-center gap-4 cursor-pointer hover:translate-y-[-10px] transition-all duration-500"
-          onClick={() => {
-            navigate("/doctors/" + item.speciality);
-            window.scrollTo(0, 0);
-          }}
-        >
-          <img
-            src={item.image}
-            id="speciality"
-            className="w-25"
-            alt={item.speciality}
-          />
-          <p className="text-gray-700 text-sm">{item.speciality}</p>
-        </div>
-      ))}
+    <div className="flex flex-col items-center gap-4 py-16 text-gray-800">
+      <div className="flex sm:justify-center gap-4 pt-5 w-full overflow-scroll">
+        {specialityData.map((item: SpecialityItem) => (
+          <div
+            key={item.id}
+            className="flex-shrink-0 flex flex-col items-center gap-4 cursor-pointer hover:translate-y-[-10px] transition-all duration-500"
+            onClick={() => {
+              navigate("/doctors/" + item.speciality);
+              window.scrollTo(0, 0);
+            }}
+          >
+            <img
+              src={item.image}
+              id="speciality"
+              className="w-25"
+              alt={item.speciality}
+            />
+            <p className="text-gray-700 text-sm">{item.speciality}</p>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
